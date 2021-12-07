217b9ada128d423bf2b7e6cc5e1cecd1b2a3c7d6
francescamassalini <94383023+francescamassalini@users.noreply.github.com>
2021-12-07 16:33:07 +0100

    Create test_driver.c

---
+++ 20211206/test_driver.c
@@ -0,0 +1,28 @@
+#include "quiz.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+int a, b;
+ if (argc != 3) { //!= significa che se i parametri che passiamo sono più di tre, ritornerà un errore
+     printf("Usage: test driver <int_a> <int_b>\n");
+     exit(-1);
+ }
+
+a = atoi(argv[1]); 
+ if (a == 0) {
+     printf("First argument must be an integer\n");
+     exit(-2);
+ }
+
+b = atoi(argv[2]);
+
+if (b == 0) {
+     printf("Second argument must be an integer\n");
+     exit(-3);
+ }
+
+printf("quiz(%i, %i) = (%i)\n", a, b, quiz(a,b));
+    return 0;
+
+}
