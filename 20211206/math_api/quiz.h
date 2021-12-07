9a1f8ccec0a6e993a270085623196806dcff07b2
francescamassalini <94383023+francescamassalini@users.noreply.github.com>
2021-12-07 16:32:03 +0100

    Create quiz.h

---
+++ 20211206/math_api/quiz.h
@@ -0,0 +1,6 @@
+#ifndef __QUIZ_H__ //se quiz.h non è definito
+#define __QUIZ_H__ //allora definisco quiz.h 
+
+int quiz(int x, int y); // definizione della funzione da chiamare e relative funzioni
+
+#endif //fine
